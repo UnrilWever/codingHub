92aaba30b3ad3e200d0cb23d373a11a79ae4a452
UnrilWever <641321483@qq.com>
2022-11-07 12:08:45 +0800

    modified:   javaScript/debounce.js 	modified:   javaScript/test.html

--- javaScript/debounce.js
+++ javaScript/debounce.js
@@ -1,9 +1,25 @@
-function debounce(cb, delay) {
-  let timer = 0
+// //最简易版
+// function debounce(cb, delay) {
+//   let timer = 0
+//   return function (...args) {
+//     if (timer) clearTimeout(timer)
+//     timer = setTimeout(() => {
+//       cb.apply(this, args)
+//     }, delay)
+//   }
+// }
+
+//加上第一次触发立即执行的功能
+function debounce(fn, delay = 50, immediate) {
+  let timer = null
   return function (...args) {
     if (timer) clearTimeout(timer)
+
+    if (immediate && !timer) {
+      fn.apply(this, args)
+    }
     timer = setTimeout(() => {
-      cb.apply(this, args)
+      fn.apply(this, args)
     }, delay)
   }
 }
