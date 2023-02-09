01060ed03c4608118425bcdc843d1e0d49e2725b
UnrilWever <641321483@qq.com>
2023-02-10 01:41:09 +0800

    new file:   "algorithm/\345\237\272\347\241\200\351\242\230\347\233\256/\344\270\215\345\220\214\347\232\204\344\272\214\345\217\211\346\220\234\347\264\242\346\240\221.js" 	renamed:    javaScript/debounce.js -> "javaScript/\351\230\262\346\212\226\343\200\201\350\212\202\346\265\201/debounce.js" 	new file:   "javaScript/\351\230\262\346\212\226\343\200\201\350\212\202\346\265\201/lodash.js" 	renamed:    javaScript/test.html -> "javaScript/\351\230\262\346\212\226\343\200\201\350\212\202\346\265\201/test.html" 	renamed:    javaScript/throttle.js -> "javaScript/\351\230\262\346\212\226\343\200\201\350\212\202\346\265\201/throttle.js"

---
+++ algorithm/基础题目/不同的二叉搜索树.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const numTrees = function (n) {
+  let dp = new Array(n + 1).fill(0)
+  dp[0] = 1
+  dp[1] = 1
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      dp[i] += dp[i - j] * dp[j - 1]
+    }
+    console.log(dp[i])
+  }
+  return dp[n]
+}
+
+console.log(numTrees(3))
